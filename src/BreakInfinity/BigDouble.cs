24717f16368c7573d349c61c95e6134d524e0cab
jensdeg <jensdegraaf88@gmail.com>
2025-10-07 20:17:06 +0200

    a start

---
+++ src/BreakInfinity/BigDouble.cs
@@ -0,0 +1,32 @@
+﻿namespace BreakInfinity;
+
+public sealed class BigDouble
+{
+    public double Mantissa { get; set; }
+
+    public int Exponent { get; set; }
+
+    public BigDouble()
+    {
+        Mantissa = 0;
+        Exponent = 0;
+    }
+
+    public BigDouble(double value)
+    {
+        if (value < 0) throw new NotImplementedException("negative numbers not supported yet");
+        if (value == 0)
+        {
+            Mantissa = 0;
+            Exponent = 0;
+        }
+
+        Exponent = (int)Math.Floor(Math.Log10(value));
+        Mantissa = Math.Round((value / Math.Pow(10, Exponent)), 2);
+    }
+
+    public override string ToString()
+    {
+        return $"{Mantissa}e{Exponent}";
+    }
+}
