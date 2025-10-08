189c087c13bb869d10cdf671a907264ecfe94867
jensdeg <jensdegraaf88@gmail.com>
2025-10-08 22:07:22 +0200

    equality and comparisons methods

--- src/BreakInfinity/BigDouble.cs
+++ src/BreakInfinity/BigDouble.cs
@@ -1,15 +1,27 @@
 ﻿namespace BreakInfinity;
 
-public sealed class BigDouble
+public readonly partial struct BigDouble
+    : IEquatable<BigDouble>,
+      IComparable<BigDouble>
 {
-    public double Mantissa { get; set; }
+    public readonly double Mantissa { get; }
 
-    public int Exponent { get; set; }
+    public readonly int Exponent { get; }
+
+    public override string ToString() => $"{Mantissa}e{Exponent}";
 
     public BigDouble()
     {
         Mantissa = 0;
         Exponent = 0;
+    }
+
+    public BigDouble(double mantissa, int exponent)
+    {
+        if (mantissa > 10) throw new NotImplementedException();
+        if (mantissa < 0 || exponent < 0) throw new NotImplementedException("negative numbers not supported yet");
+        Mantissa = mantissa;
+        Exponent = exponent;
     }
 
     public BigDouble(double value)
@@ -19,14 +31,31 @@ public sealed class BigDouble
         {
             Mantissa = 0;
             Exponent = 0;
+            return;
         }
 
         Exponent = (int)Math.Floor(Math.Log10(value));
         Mantissa = Math.Round((value / Math.Pow(10, Exponent)), 2);
     }
 
-    public override string ToString()
+    public bool Equals(BigDouble other)
+        => Mantissa == other.Mantissa && Exponent == other.Exponent;
+
+    public override bool Equals(object? obj)
+        => obj is BigDouble other && Equals(other);
+
+    public override int GetHashCode()
+        => HashCode.Combine(Mantissa, Exponent);
+
+    public int CompareTo(BigDouble other)
     {
-        return $"{Mantissa}e{Exponent}";
+        if (Exponent != other.Exponent) return Exponent.CompareTo(other.Exponent);
+        return Mantissa.CompareTo(other.Mantissa);
     }
+
+    public static bool operator ==(BigDouble left, BigDouble right)
+        => left.Equals(right);
+
+    public static bool operator !=(BigDouble left, BigDouble right)
+        => !(left == right);
 }
---
+++ src/BreakInfinity/BigDoubleMathOperations.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+
+namespace BreakInfinity;
+
+public readonly partial struct BigDouble
+    : IAdditionOperators<BigDouble, BigDouble, BigDouble>, IAdditiveIdentity<BigDouble, BigDouble>,
+      IMultiplyOperators<BigDouble, BigDouble, BigDouble>, IMultiplicativeIdentity<BigDouble, BigDouble>,
+      IDivisionOperators<BigDouble, BigDouble, BigDouble>,
+      ISubtractionOperators<BigDouble, BigDouble, BigDouble>,
+      IModulusOperators<BigDouble, BigDouble, BigDouble>,
+      IDecrementOperators<BigDouble>,
+      IIncrementOperators<BigDouble>,
+      IMinMaxValue<BigDouble>
+{
+    public static BigDouble AdditiveIdentity => new();
+
+    public static BigDouble MultiplicativeIdentity => new(1);
+
+    public static BigDouble MaxValue => new(9.99, 9999);
+
+    public static BigDouble MinValue => new(0);
+
+    public static BigDouble operator +(BigDouble left, BigDouble right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BigDouble operator -(BigDouble left, BigDouble right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BigDouble operator ++(BigDouble value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BigDouble operator --(BigDouble value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BigDouble operator *(BigDouble left, BigDouble right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BigDouble operator /(BigDouble left, BigDouble right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BigDouble operator %(BigDouble left, BigDouble right)
+    {
+        throw new NotImplementedException();
+    }
+}
